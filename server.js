9d2e9cafdc5652bdafa5c4e36d4f9b4989e33423
Jason Crawford <daxapps777@gmail.com>
2017-06-07 16:00:31 -0500

    3 hrs

---
+++ server.js
@@ -0,0 +1,81 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+const {PORT, DATABASE_URL} = require('./config');
+const {Blog} = require('./models');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.get('/blog-posts', (req, res) => {
+	Blog
+		.find()
+		.exec()
+		.then(blogposts => {
+			res.json({
+				blogposts: blogposts.map(
+					(blogposts) => blogposts.apiRepr())
+			});
+		})
+		.catch(
+			err => {
+				console.error(err);
+				res.status(500).json({message: 'Internal server error'});
+			});
+});
+
+app.use('*', function(req, res) {
+	res.status(404).json({message: 'Not Found'});
+});
+
+let server;
+
+function runServer(databaseUrl=DATABASE_URL, port=PORT) {
+	return new Promise((resolve, reject) => {
+		mongoose.connect(databaseUrl, err => {
+			if (err) {
+				return reject(err);
+			}
+			server = app.listen(port, () => {
+				console.log(`Your app is listening on port ${port}`);
+				resolve();
+			})
+			.on('error', err => {
+				mongoose.discounnect();
+				reject(err);
+			});
+		});
+	});
+}
+
+function closeServer() {
+	return mongoose.disconnect().then(() => {
+		return new Promise((resolve, reject) => {
+			console.log('Closing server');
+			server.close(err => {
+				if (err) {
+					return reject(err);
+				}
+				resolve();
+			});
+		});
+	});
+}
+
+if (require.main === module) {
+	runServer().catch(err => console.error(err));
+};
+
+module.exports = {app, runServer, closeServer};
+
+
+
+
+
+
+
+
+
